02fb72431ec2eabd89752ff62bdabe3b030fb5ca
bd65 <bd65@students.waikato.ac.nz>
2025-05-03 21:17:57 +1200

    basic code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,54 @@
 // src/index.ts
-export function greet(name: string): string {
-    return `Hello, ${name}!`;
-  }
+
+type CommandMetadata = {
+    name: string;
+    description: string;
+    icon: string;
+}
+
+type CommandBinding<ReturnType> = {
+    metadata: CommandMetadata;
+    run: () => ReturnType;
+}
+
+type CommandBindpoint<Args extends any[], ReturnType> = {
+    (...args: Args): ReturnType | undefined;
+    argBindings: Record<string, CommandBinding<ReturnType>>;
+}
+
+export function defineCommand<Args extends any[], ReturnType extends any>(): CommandBindpoint<Args> {
+
+    const command: CommandBindpoint<Args,ReturnType> = Object.assign(
+        (...args: Args) => {
+            console.log("Executing command")
+            // Use the arguments to create a unique key
+            const key = args.map(arg => arg.toString()).join('.');
+            // Find the command in the argBindings,
+            // and run it if it exists.
+            return command.argBindings[key]?.run() || undefined;
+        },
+        {
+            argBindings: {} as Record<string, CommandBinding<ReturnType>>
+        }
+    )
+
+    return command;
+}
+
+export function addBinding<Args extends any[], ReturnType>(command: CommandBindpoint<Args, ReturnType>, metadata: CommandMetadata, fn: () => ReturnType, ...args: Args): string {
+    
+    // Use the arguments to create a unique key
+    const key = args.map(arg => arg.toString()).join('.');
+    // Store the function in the argBindings object
+    command.argBindings[key] = { metadata: metadata, run: fn };
+    return key;
+}
+
+export function removeBinding<Args extends any[], ReturnType>(command: CommandBindpoint<Args, ReturnType>, key: string) {
+    // Remove the function from the argBindings object
+    delete command.argBindings[key];
+}
+
+
+
+
--- tests/greet.test.ts
+++ tests/greet.test.ts
@@ -1,9 +1,21 @@
 // tests/greet.test.ts
 import { describe, it, expect } from 'vitest';
-import { greet } from '../src';
+import { defineCommand, addBinding } from '../src';
 
-describe('greet', () => {
-  it('greets by name', () => {
-    expect(greet('Alice')).toBe('Hello, Alice!');
+
+describe('add command', () => {
+  it('adds a command', () => {
+    const command = defineCommand();
+    const fn = () => 'Hello, world!';
+    const args = ['arg1', 'arg2'];
+    const key = addBinding(command,{
+        name: 'greet',
+        description: 'Greet the world',
+        icon: '🌍'
+    }, fn, ...args);
+    // check the command works
+    expect(command('arg1', 'arg2')).toBe('Hello, world!');
+    // check the command details can be accessed.
+    expect(command.argBindings[key].metadata.name).toBe('greet');
   });
-});
+})

9edf2b30c239f64dda3bcf0a7aaef4aca56d9951
bd65 <bd65@students.waikato.ac.nz>
2025-05-07 14:36:50 +1200

    intellisense for library recognition

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import {CommandProvider} from 'just-search-it'
 
 function App() {
 
   return (
-    <>
+    <CommandProvider>
       <h1>Vite + React</h1>
       <div className="card">
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-    </>
+    </CommandProvider>
   )
 }
 
